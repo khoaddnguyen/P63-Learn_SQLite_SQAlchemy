0317d80ab8179f00b28a7cd4d8533f4f98e6864f
kwa_laa <khoaddnguyen@gmail.com>
2023-09-25 14:28:11 -0500

    create a table and a new record

--- main.py
+++ main.py
@@ -27,7 +27,23 @@ db = SQLAlchemy()
 # initialize the app with the extension
 db.init_app(app)
 
+# Create table
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+
+    # Optional: this will allow each book object to be identified by its title when printed.
+    def __repr__(self):
+        return f'<Book {self.title}>'
 
 # Create table schema in the database. Requires application context
 with app.app_context():
     db.create_all()
+
+# Create a record
+with app.app_context():
+    new_book = Book(id=1, title="Harry Potter", author="J. K. Rowling", rating=9.3)
+    db.session.add(new_book)
+    db.session.commit()
